a9bc663b9441ceaf7ce314b7a1e4c09ca6d5d612
Navya Rathore <rathorenavya45@gmail.com>
2025-11-17 18:44:37 +0530

    add models

---
+++ models/Task.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+const taskSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: [true, 'Task title is required'],
+    trim: true
+  },
+  description: {
+    type: String,
+    required: [true, 'Task description is required'],
+    trim: true
+  },
+  status: {
+    type: String,
+    enum: ['pending', 'in-progress', 'completed'],
+    default: 'pending'
+  }
+}, {
+  timestamps: true
+});
+
+module.exports = mongoose.model('Task', taskSchema);
